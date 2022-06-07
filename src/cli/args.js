53d08903e2d22f5c107b852f04dd392bb02eee76
Labunin Oleg (Oleg) **CTR** <olabunin@avaya.com>
2022-06-07 12:51:10 +0300

    feat: added cli solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,9 @@
 export const parseArgs = () => {
-    // Write your code here 
-};
+    // Write your code here
+    const args = process.argv.slice(2);
+    args.map((item, i, arr) => {
+        if (item.includes('--')) {
+            console.log(`${item} is ${arr[i + 1]}`);
+        }
+    });
+}
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,9 @@
 export const parseEnv = () => {
     // Write your code here 
+    Object.entries(process.env).map(([key, value]) => {
+        if (key.includes('RSS_')) {
+            console.log(`${key}=${value}`)
+        }
+    })
 };
+
