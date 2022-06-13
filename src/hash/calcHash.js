e4906684710395d5465ff2e7ccb1bff57bc6934d
Labunin Oleg (Oleg) **CTR** <olabunin@avaya.com>
2022-06-13 18:35:24 +0300

    feat: add calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,19 @@
+import { readFile } from 'fs';
+import { pathToFile } from '../pathToFile.js';
+import { createHash } from 'node:crypto';
+
 export const calculateHash = async () => {
     // Write your code here 
+
+    readFile(
+        pathToFile(import.meta.url, 'files', 'fileToCalculateHashFor.txt'),
+        'utf-8',
+        (err, text) => {
+            if (err) return console.error('FS operation failed');
+            const hash = createHash('sha256').update(text).digest('hex');
+            console.log(hash)
+        }
+    );
 };
+
+calculateHash();
