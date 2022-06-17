184be81c957e704bab8463fd01582a05a457482b
Labunin Oleg (Oleg) **CTR** <olabunin@avaya.com>
2022-06-17 17:07:40 +0300

    feat: add zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,24 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pathToFile } from '../pathToFile.js';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+
 export const compress = async () => {
     // Write your code here 
+    const sourceFile = pathToFile(import.meta.url, 'files', 'fileToCompress.txt')
+    const input = createReadStream(sourceFile, 'utf-8');
+    const destinationFile = pathToFile(import.meta.url, 'files', 'archive.gz');
+    const output = createWriteStream(destinationFile);
+    const gzip = createGzip();
+    pipeline(
+        input,
+        gzip,
+        output,
+        err => {
+            if (err) {
+                console.log('Error', err.message)
+            }
+        }
+    );
 };
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,25 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pathToFile } from '../pathToFile.js';
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+
 export const decompress = async () => {
     // Write your code here 
+    createUnzip
+    const sourceFile = pathToFile(import.meta.url, 'files', 'archive.gz');
+    const input = createReadStream(sourceFile);
+    const destinationFile = pathToFile(import.meta.url, 'files', 'fileToCompress2.txt');
+    const output = createWriteStream(destinationFile, 'utf-8');
+    const unzip = createUnzip();
+    pipeline(
+        input,
+        unzip,
+        output,
+        err => {
+            if (err) {
+                console.log('Error', err.message)
+            }
+        }
+    );
 };
+decompress();
