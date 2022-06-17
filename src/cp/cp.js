3e194eb4d1e1cb47cb28c22e9d99aa2a5ab8d7e0
Labunin Oleg (Oleg) **CTR** <olabunin@avaya.com>
2022-06-18 01:17:42 +0300

    feat: add child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,17 @@
+import { pathToFile } from '../pathToFile.js';
+import { spawn } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
     // Write your code here
+
+    const childProcess = spawn('node', [pathToFile(import.meta.url, 'files', 'script.js'), ...args.split(' ')]);
+
+    process.stdin.on('data', value => {
+        childProcess.stdin.write(value);
+    });
+
+    childProcess.stdout.on('data', value => {
+        console.log(value.toString())
+    })
 };
+spawnChildProcess('--arg1 --arg2')
